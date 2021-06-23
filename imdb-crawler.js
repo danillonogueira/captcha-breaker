d42b190ddd829ff471496f83db21cc98e5378739
Danillo Nogueira <dnll_nog@yahoo.com>
2021-06-23 17:42:26 -0300

    first commit

---
+++ imdb-crawler.js
@@ -0,0 +1,37 @@
+// https://www.imdb.com/chart/top/
+
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const options = { 
+  headless: false,
+};
+
+const writeTxt = (movies) => {
+  movies.forEach((movie) => fs.appendFileSync('movies.txt', `${movie}\n`))
+};
+
+(async () => {
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+
+  console.log('Browsing...')
+  await page.goto('https://www.imdb.com/chart/top/');
+
+  console.log('Getting movies...')
+  await page.$$eval('.lister-list tr .titleColumn a', (movies) => {
+    const _movies = (movies.map(movieEl => movieEl.textContent));
+
+    return new Promise((resolve, reject) => {
+      if (_movies.length) {
+        resolve(_movies);
+      }
+
+      reject();
+    });
+  })
+    .then((movies) => {
+      console.log(movies);
+      // writeTxt(movies);
+    });
+})();
---
+++ linkedin-crawler.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+
+require('dotenv').config();
+
+const options = { 
+  headless: false,
+  slowMo: 50
+};
+
+(async () => {require('dotenv').config();
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+
+  console.log('Navigating to LinkedIn');
+  await Promise.all([
+    page.waitForNavigation(),
+    page.goto('https://www.linkedin.com/home')
+  ]);
+
+  console.log('Clicking sign in button');
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('.nav__button-secondary')
+  ]);
+
+  console.log('Typing email...')
+  await page.type('#username', process.env.EMAIL);
+
+  console.log('Typing password...')
+  await page.type('#password', process.env.PASSWORD);
+
+  console.log('Logging in...')
+  await page.click('.btn__primary--large');
+
+  await Promise.all([
+    page.waitForNavigation(),
+    console.log('SUCCESS')
+  ]);
+})();
